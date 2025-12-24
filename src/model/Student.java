15eb4f0c5523d64a7d368270c12415b2e4295548
Tahmid Saif <tahmid.saif@stonybrook.edu>
2025-12-24 09:55:29 -0500

    Day 4: introduce Student and StudentManager class

---
+++ src/model/Student.java
@@ -0,0 +1,29 @@
+package model;
+
+public class Student {
+    private int id;
+    private String name;
+    private int score;
+
+    public Student(){
+    }
+
+    public Student(int id, String name, int score){
+        this.id = id;
+        this.name = name;
+        this.score = score;
+    }
+
+    public int getId(){
+        return id;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
+    }
+}
+
---
+++ src/model/StudentManager.java
@@ -0,0 +1,14 @@
+package model;
+
+public class StudentManager {
+    private Student[] student;
+    private int size;
+    
+    public StudentManager(){
+
+    }
+
+    public int getSize(){
+        return size;
+    }
+}
